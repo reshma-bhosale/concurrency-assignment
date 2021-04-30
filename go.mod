a312cf426168833efe080a2048eaea2d01af695e
Reshma.Bhosale <reshmab@mkcl.org>
2021-04-30 10:00:03 +0530

    first commit

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/reshma-bhosale/go-project.git
+
+go 1.16
+
+require (
+	github.com/gin-gonic/gin v1.7.1
+	go.mongodb.org/mongo-driver v1.5.1 // indirect
+)
---
+++ main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"github.com/gin-gonic/gin"
+	"log"
+	"time"
+	"go.mongodb.org/mongo-driver/bson"
+    "go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"go.mongodb.org/mongo-driver/mongo/readpref"
+
+)
+
+type Result struct {
+	Url    string `json:"url"` //Bind result to display json
+	Length int    `json:"response_size"`
+}
+
+func main() {
+	r := gin.Default()
+	r.POST("/check", handle)
+	r.Run(":8000")
+
+}
+
+func handle(c *gin.Context) {
+
+=	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	client, err := mongo.Connect(ctx, options.Client().ApplyURI("mongodb://127.0.0.1:27017"))
+	if err != nil {
+		panic(err)
+	}
+	defer func() {
+		if err = client.Disconnect(ctx); err != nil {
+			panic(err)
+		}
+	}()
+	if err := client.Ping(ctx, readpref.Primary()); err != nil {
+		panic(err)
+	}
+	fmt.Println("Successfully connected and pinged.")
+
+	var result Result
+	c.Bind(&result)
+	ch := make(chan int)
+	url  := "https://www." + result.Url
+	go responseSize(url, ch)
+	result.Url = url
+	result.Length = <-ch
+
+	response := client.Database("go-training").Collection("responses")
+	res, err := response.InsertOne(context.Background(), bson.D{{"Url" , result.Url}, {"Response size" , result.Length}})
+	if err != nil {
+		log.Fatal(err)
+	}
+	c.JSON(200, result)
+	fmt.Println("Document %v added successfully ", *res)
+
+}
+
+func responseSize(url string, channel chan int) {
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Something Bad Happened!!", err)
+		return
+	}
+	defer response.Body.Close()
+	body, _ := ioutil.ReadAll(response.Body)
+	channel <- len(body)
+
+}
